d7142bed81c62257dfcffdfc8a3ad42e9f08baa8
verduttio <bszwaja20@gmail.com>
2022-12-21 13:33:47 +0100

    Added basic test (assert logic) with building it (only tests) in github actions.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include <boost/filesystem/operations.hpp>
+//#include <boost/filesystem/operations.hpp>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    std::cout << "The size of " << boost::filesystem::absolute(argv[0])
-              << " is " << boost::filesystem::file_size(argv[0]) << '\n';
-
+//    std::cout << "The size of " << boost::filesystem::absolute(argv[0])
+//              << " is " << boost::filesystem::file_size(argv[0]) << '\n';
+//
     std::cout << "Hello world ;)" << std::endl;
     std::cout << "It works, finally..." << std::endl;
     return 0;
---
+++ tests/simpleTest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cassert>
+
+int main() {
+    assert(2+2==4);
+    assert(2+2==5);
+
+    return 0;
+}
